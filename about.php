11c249f874078fb3fdf782f08d7835273be2ae3b
sorenlhansen <sorenlind94@hotmail.com>
2016-09-26 13:54:05 +0200

    Update files
    filer opdateret

--- about.php
+++ about.php
@@ -1,4 +1,4 @@
-   <?php Require('navmenu.php')  ?>
+   <?php Require('navmenu.php')  // henter navmenu.php så den fungere som header/menu?>
    
        <h1> About side </h1>
 	<p>Some text about me</p>
--- navmenu.php
+++ navmenu.php
@@ -1,18 +1,18 @@
-<?php require ('database_fb.php') ?>
+<?php require ('database_fb.php')  // henter connection til one.coms webserver ?>
 <!doctype html>
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="Menu.css">
 <meta charset="utf-8">
-<?php $curpage = basename($_SERVER['PHP_SELF']); ?>
-<title><?php echo "Menu- $curpage"?> // Titlen bliver til "Menu - " og den aktive side</title>
+<?php $curpage = basename($_SERVER['PHP_SELF']);  //definere $curpage som den aktive side.?>
+<title><?php echo "Menu- $curpage"?> <!--Titlen bliver til "Menu - " og den aktive side--></title>
 </head>
 
 
 <body>
 	<nav>
 		<ul>
-        	<li><a href="hjem.php" <?php if ($curpage == 'hjem.php'){echo'class="active"';} ?>>Home</a></li>
+        	<li><a href="hjem.php" <?php if ($curpage == 'hjem.php'){echo'class="active"';} // Hvis $curpage er hjem.php skrives der class="active" i anchor. Dette gør, at den aktive side bliver stylet fra CSS'en ?>>Home</a></li>
         	<li><a href="about.php" <?php if($curpage == 'about.php'){echo'class="active"';} ?>>About</a></li>
     	    <li><a href="art.php" <?php if ($curpage == 'art.php'){echo'class="active"';} ?>>Art</a></li>
   	    </ul>
